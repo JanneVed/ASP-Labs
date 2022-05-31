d725106a60c0c0f218c87a6f2bf0258a522aab57
Jan Vedin <janven.jv@gmail.com>
2022-05-31 11:39:36 +0200

    Created Lab 4 files

---
+++ Lab_4_MVC_Razor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_4_MVC_Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
